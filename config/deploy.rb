9855e5e8ba6682dc8dac870c486af04870080c42
Tomwey <tomwey@163.com>
2015-08-10 22:04:08 +0800

    update deploy configs

--- config/deploy.rb
+++ config/deploy.rb
@@ -81,7 +81,8 @@ namespace :deploy do
   
   # As of Capistrano 3.1, the `deploy:restart` task is not called
   # automatically.
-  after 'deploy:publishing', 'deploy:restart'
+  # after 'deploy:publishing', 'deploy:restart'
+  after :finishing, 'deploy:restart'
   
 end
 
